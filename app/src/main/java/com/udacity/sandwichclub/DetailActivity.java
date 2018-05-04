5fb24b7d28483be42c450694283cab3ab70d8c33
skippyjonney <jodgarci@gmail.com>
2018-05-04 12:57:12 -0700

    Finish UI Population

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -4,11 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
+
+import java.util.List;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -46,7 +49,7 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
@@ -59,7 +62,37 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateUI(Sandwich sandwich) {
+
+        // Text View References
+        TextView tv_AKA = findViewById(R.id.also_known_tv);
+        TextView tv_Origin = findViewById(R.id.origin_tv);
+        TextView tv_Ingredients = findViewById(R.id.ingredients_tv);
+        TextView tv_Description = findViewById(R.id.description_tv);
+
+        List<String> listAKA = sandwich.getAlsoKnownAs();
+        if(listAKA.size() > 0) {
+            for(int i = 0; i < listAKA.size(); i++) {
+                tv_AKA.append(" " + listAKA.get(i));
+                if(i != listAKA.size() - 1) {
+                    tv_AKA.append(",");
+                }
+            }
+        }
+
+        tv_Origin.setText("  " + sandwich.getPlaceOfOrigin());
+
+        List<String> listIngredients = sandwich.getIngredients();
+        if(listIngredients.size() > 0) {
+            for(int i = 0; i < listIngredients.size(); i++) {
+                tv_Ingredients.append(" " + listIngredients.get(i));
+                if(i != listIngredients.size() - 1) {
+                    tv_Ingredients.append(",");
+                }
+            }
+        }
+
+        tv_Description.setText(sandwich.getDescription());
 
     }
 }
